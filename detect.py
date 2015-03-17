dd5ec56d159d121fdea1f53e24e003db3b032ba9
L.bdcdb <ludo.bouan@gmail.com>
2015-03-17 17:07:35 +0100

    communication avec moteur

--- detect.py
+++ detect.py
@@ -1,7 +1,16 @@
 import cv2
 import sys
+import serial
+ser = serial.Serial('/dev/cu.usbmodem1411', 115200) # Establish the connection on a specific port
 
 def findfaces(grayframe):
+    '''
+    Description: Finds the faces in the frame
+    -----------------------------------------
+    Input: image in grayscale
+    Output: List of tuples (x, y, w, h)
+    -----------------------------------------
+    '''
     return faceCascade.detectMultiScale(
                 grayframe,
                 scaleFactor=1.1, 
@@ -12,6 +21,13 @@ def findfaces(grayframe):
 
 
 def findeyes (grayframe):
+    '''
+    Description: Finds the eyes in the frame
+    ----------------------------------------
+    Input: image in grayscale
+    Output: List of tuples (x, y, w, h)
+    ----------------------------------------
+    '''
     return eyesCascade.detectMultiScale(
                     grayframe,
                     scaleFactor=1.3,
@@ -21,27 +37,47 @@ def findeyes (grayframe):
                 )
 
 def disp(frame, faces, eyes):
-        if len(faces) == 0:
+    '''
+    Description: Draws colored boxes around eyes and faces
+    ----------------------------------------
+    Input: image; list of tuples of faces (x, y, w, h); list of tuples of eyes (x, y, w, h)
+    ----------------------------------------
+    '''
+    if len(faces) == 0:
+        for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
+    else :
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            roi_color = frame[y:y+h, x:x+w]
             for(ex,ey,ew,eh) in eyes:
-                cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
-        else :
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                roi_color = frame[y:y+h, x:x+w]
-                for(ex,ey,ew,eh) in eyes:
-                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
 
 def center(width, height):
+    '''
+    Description: finds center of video
+    ----------------------------------------
+    Input: width (int); height (int)
+    Output: tuple of center (x,y)
+    ----------------------------------------
+    '''
     return (width/2, height/2)
 
 def offset(centers, elements):
+    '''
+    Description: finds offset between center and elements
+    ----------------------------------------
+    Input: centers (tuple), elements (list of tulpes)
+    Output: list of offsets (list of tuples)
+    ----------------------------------------
+    '''
     xoff = list()
     yoff = list()
     for (x, y, w, h) in elements:
         xoff = xoff + [x+(w/2)-centers[0]]
     for (x, y, w, h) in elements:
         yoff = yoff + [y+(h/2)-centers[1]]
-    return zip(yoff,xoff)
+    return zip(xoff,yoff)
 
 def face_filter():
     '''
@@ -56,26 +92,35 @@ def face_filter():
 
 def mvt_filter(offset):
     '''
-    Only instruct motor to turn if offset > x
+    Description: returns offset if above sensibility threshold
+    ----------------------------------------
+    Input: offset (tuple)
+    Output: offset (tuple) or None
+    ----------------------------------------
     '''
     L = list()
     for i in range(len(offset)):
         if len(offset[i]) > 0:
-            if abs(offset[i][0]) > 30 or abs(offset[i][1]) > 30:
+            if abs(offset[i][0]) > 40 or abs(offset[i][1]) > 40:
                 L = L + [offset[i]]
     if L != list (): return L
 
-def main(a_intvl,width, height, display):
+def send_arduino(angle):
+    ser.write(str(angle)+"\n")
+    #Serial.print(str(angle)+"\n")
+
+def main(a_intvl,width, height, display, angle):
     # Create the haar cascade
     global faceCascade
     global eyesCascade
     faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     eyesCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-    print(width)
-    print(height)
     w = width
     h = height
+    print(w)
+    print(h)
+
 
     vid = cv2.VideoCapture(0)
     vid.set(3,w) # Defines video width
@@ -110,7 +155,10 @@ def main(a_intvl,width, height, display):
                     eyes = findeyes(fsection_grayframe)
 
             f = mvt_filter(offset(center(w, h), faces))
-            if f: print f
+            if f: 
+                print f
+                if f[0][0] > 0: angle -= 20; send_arduino(angle)
+                if f[0][0] < 0: angle += 20; send_arduino(angle)
 
             analyse += 1
 
@@ -132,6 +180,7 @@ def main(a_intvl,width, height, display):
             break
 
     # When everything is done, release the capture
+    ser.close()
     video_capture.release()
     cv2.destroyAllWindows()
 
@@ -145,6 +194,9 @@ if __name__ == "__main__":
     except: width = 600
     try: height = (int(sys.argv[4]))
     except: height = 480
+    angle = 90
+    ser.write(str(angle)+"\n")
 
-    main(a_intvl, width, height, display)
+
+    main(a_intvl, width, height, display, angle)
 
